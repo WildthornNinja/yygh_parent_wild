e539bb90fdb74e7e5a372e3c767ae05c52ede0db
WildThorn <1343578324@qq.com>
2022-11-20 11:03:14 +0800

    搭建预约挂号系统父工程，并修改pom.xml配置文件，锁定各依赖版本号(￣▽￣)~*

---
+++ src/main/java/com/wild/yygh_parent_wild/YyghParentWildApplication.java
@@ -0,0 +1,13 @@
+package com.wild.yygh_parent_wild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YyghParentWildApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YyghParentWildApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wild/yygh_parent_wild/YyghParentWildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wild.yygh_parent_wild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YyghParentWildApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
