026118634752c522d49222f00f164ac4cf136eb1
WildThorn <1343578324@qq.com>
2022-11-20 17:01:58 +0800

    com.wild.yygh.common.config路径下新建Swagger2Config配置类 注意: 需要引入common模块资源，需要在启动类上添加 @ComponentScan(basePackages = "com.wild")注解，以供扫描[]~(￣▽￣)~*

--- service/service_hosp/src/main/java/com/wild/yygh/hosp/ServiceHospApplication.java
+++ service/service_hosp/src/main/java/com/wild/yygh/hosp/ServiceHospApplication.java
@@ -2,8 +2,10 @@ package com.wild.yygh.hosp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.wild")
 public class ServiceHospApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceHospApplication.class, args);
--- service/service_hosp/src/main/java/com/wild/yygh/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/wild/yygh/hosp/controller/HospitalSetController.java
@@ -3,6 +3,8 @@ package com.wild.yygh.hosp.controller;
 import com.baomidou.mybatisplus.extension.api.R;
 import com.wild.yygh.hosp.service.HospitalSetService;
 import com.wild.yygh.model.hosp.HospitalSet;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,7 @@ import java.util.List;
 
 @RestController //医院设置接口
 @RequestMapping("/admin/hosp/hospitalSet")
+@Api(tags = "首页模块")
 public class HospitalSetController {
     @Autowired
     private HospitalSetService hospitalSetService;
@@ -19,10 +22,10 @@ public class HospitalSetController {
     /**
      * 查询所有医院设置
      */
+    //@ApiOperation(value = "医院设置列表")
     @GetMapping("/findAll")
     public List<HospitalSet> findAll() {
         List<HospitalSet> hospitalSetList = hospitalSetService.list();
         return hospitalSetList;
     }
-
 }
