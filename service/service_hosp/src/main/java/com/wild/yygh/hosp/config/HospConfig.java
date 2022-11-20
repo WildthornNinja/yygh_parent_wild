f54d035e0862d9ab539a5a2a5db27f243927c2b7
WildThorn <1343578324@qq.com>
2022-11-20 18:35:51 +0800

    在HospConfig配置类中添加分页插件(ﾉ´▽｀)ﾉ♪

--- service/service_hosp/src/main/java/com/wild/yygh/hosp/config/HospConfig.java
+++ service/service_hosp/src/main/java/com/wild/yygh/hosp/config/HospConfig.java
@@ -1,11 +1,20 @@
 package com.wild.yygh.hosp.config;
 
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootConfiguration
 @EnableTransactionManagement //开启事务管理
 @MapperScan("com.wild.yygh.hosp.mapper")
 public class HospConfig {
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
 }
