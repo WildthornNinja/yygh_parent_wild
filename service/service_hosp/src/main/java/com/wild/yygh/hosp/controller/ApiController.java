bf2df5b6779d16ba860d15a12a02e06bec3a18e7
WildThorn <1343578324@qq.com>
2022-11-27 23:43:59 +0800

    完成两端接口之间的签名校验(*’U`*)

--- service/service_hosp/src/main/java/com/wild/yygh/hosp/controller/ApiController.java
+++ service/service_hosp/src/main/java/com/wild/yygh/hosp/controller/ApiController.java
@@ -1,8 +1,11 @@
 package com.wild.yygh.hosp.controller;
 
 import com.wild.yygh.common.Result;
+import com.wild.yygh.common.exception.YyghException;
 import com.wild.yygh.hosp.service.HospitalService;
+import com.wild.yygh.hosp.service.HospitalSetService;
 import com.wild.yygh.hosp.utils.HttpRequestHelper;
+import com.wild.yygh.hosp.utils.MD5;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,17 +22,37 @@ import java.util.Map;
 public class ApiController {
     @Autowired
     private HospitalService hospitalService;
+    @Autowired
+    private HospitalSetService hospitalSetService;
 
     @ApiOperation(value = "上传医院数据")
     @PostMapping("/saveHospital")
     public Result saveHospital(HttpServletRequest request){
-        //1获取并转化参数
+        //1.获取并转化参数
         Map<String, String[]> parameterMap = request.getParameterMap();
         Map<String, Object> paramMap = HttpRequestHelper.switchMap(parameterMap);
-        //2 TODO 签名校验
-        //3调用方法存储数据
+        //2.签名校验
+            //2.1 获取医院签名
+        String hoscode = (String) paramMap.get("hoscode");
+        String sign = (String) paramMap.get("sign");
+            //2.2 调用接口获取自己本地签名
+        String hospSign = hospitalSetService.getSignKey(hoscode);
+            //2.3本地签名使用MD5加密
+        String hospSignMD5 = MD5.encrypt(hospSign);
+        System.out.println("sign = " + sign);
+        System.out.println("hospSignMD5 = " + hospSignMD5);
+            //2.4 签名校验
+        if(!hospSignMD5.equals(sign)){
+            throw new YyghException(20001,"签名校验失败");
+        }
+        //传输过程中“+”转换为了“ ”，因此我们要转换回来
+        String logoData = (String)paramMap.get("logoData");
+        logoData = logoData.replaceAll(" ","+");
+        paramMap.put("logoData",logoData);
+
+        //3.调用方法存储数据
         hospitalService.saveHospital(paramMap);
-        //4返回结果
+        //4.返回结果
         return Result.ok();
     }
 
--- service/service_hosp/src/main/java/com/wild/yygh/hosp/service/impl/HospitalSetServiceImpl.java
+++ service/service_hosp/src/main/java/com/wild/yygh/hosp/service/impl/HospitalSetServiceImpl.java
@@ -1,6 +1,8 @@
 package com.wild.yygh.hosp.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.wild.yygh.common.exception.YyghException;
 import com.wild.yygh.hosp.mapper.HospitalSetMapper;
 import com.wild.yygh.hosp.service.HospitalSetService;
 import com.wild.yygh.model.hosp.HospitalSet;
@@ -11,5 +13,21 @@ import org.springframework.stereotype.Service;
 public class HospitalSetServiceImpl extends ServiceImpl<HospitalSetMapper,HospitalSet> implements HospitalSetService{
     @Autowired
     private HospitalSetMapper hospitalSetMapper;
+    /**
+     * 获取签名key
+     * @param hoscode
+     * @return
+     */
+    @Override
+    public String getSignKey(String hoscode) {
+        QueryWrapper<HospitalSet> wrapper = new QueryWrapper<>();
+        wrapper.eq("hoscode",hoscode);
+        HospitalSet hospitalSet = baseMapper.selectOne(wrapper);
+        if(hospitalSet!=null){
+            return hospitalSet.getSignKey();
+        }else {
+            throw new YyghException(20001,"获取签名失败");
+        }
+    }
 }
 
