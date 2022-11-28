ba74eeb801bd44064550de2e1cb8ebe53cb16174
WildThorn <1343578324@qq.com>
2022-11-28 22:43:32 +0800

    server_hosp模块、server_cmn添加到Nacos注册中心Ｏ(≧▽≦)Ｏ

--- service/service_cmn/src/main/java/com/wild/yygh/cmn/ServiceCmnApplication.java
+++ service/service_cmn/src/main/java/com/wild/yygh/cmn/ServiceCmnApplication.java
@@ -2,10 +2,12 @@ package com.wild.yygh.cmn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.wild")
+@EnableDiscoveryClient //开启注解 ，表明注册中心客户端
 public class ServiceCmnApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceCmnApplication.class,args);
--- service/service_hosp/src/main/java/com/wild/yygh/hosp/ServiceHospApplication.java
+++ service/service_hosp/src/main/java/com/wild/yygh/hosp/ServiceHospApplication.java
@@ -2,10 +2,12 @@ package com.wild.yygh.hosp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.wild")
+@EnableDiscoveryClient //开启注解 ，表明注册中心客户端
 public class ServiceHospApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceHospApplication.class, args);
