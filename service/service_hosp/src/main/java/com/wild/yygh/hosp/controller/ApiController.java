937b9e1f3e10275761ab1047d80d162e3ffe3693
WildThorn <1343578324@qq.com>
2022-11-28 19:55:41 +0800

    将签名校验代码抽取出公共部分以供方便调用(ღ˘⌣˘ღ)

--- service/service_hosp/src/main/java/com/wild/yygh/hosp/controller/ApiController.java
+++ service/service_hosp/src/main/java/com/wild/yygh/hosp/controller/ApiController.java
@@ -36,21 +36,13 @@ public class ApiController {
         //2.签名校验
             //2.1 获取医院签名
         String hoscode = (String) paramMap.get("hoscode");
+        String sign = (String) paramMap.get("sign");
         //参数校验
         if(StringUtils.isEmpty(hoscode)){
             throw new YyghException(20001,"失败");
         }
-        String sign = (String) paramMap.get("sign");
-            //2.2 调用接口获取自己本地签名
-        String hospSign = hospitalSetService.getSignKey(hoscode);
-            //2.3本地签名使用MD5加密
-        String hospSignMD5 = MD5.encrypt(hospSign);
-        System.out.println("sign = " + sign);
-        System.out.println("hospSignMD5 = " + hospSignMD5);
-            //2.4 签名校验
-        if(!hospSignMD5.equals(sign)){
-            throw new YyghException(20001,"签名校验失败");
-        }
+        //2.4 签名校验
+        checkSign(hoscode,sign);
         //传输过程中“+”转换为了“ ”，因此我们要转换回来
         String logoData = (String)paramMap.get("logoData");
         logoData = logoData.replaceAll(" ","+");
@@ -73,18 +65,24 @@ public class ApiController {
             throw new YyghException(20001,"失败");
         }
         //3.签名校验
-        //调用接口获取自己本地签名
-        String hospSign = hospitalSetService.getSignKey(hoscode);
-        //本地签名使用MD5加密
-        String hospSignMD5 = MD5.encrypt(hospSign);
-        //2.4 签名校验
-        if(!hospSignMD5.equals(sign)){
-            throw new YyghException(20001,"签名校验失败");
-        }
+        checkSign(hoscode,sign);
         //4.调用接口查询信息
         Hospital hospital = hospitalService.getHospByHoscode(hoscode);
         return Result.ok(hospital);
 
+    }
+    /**
+     * 抽取公共签名校验
+     */
+    public void checkSign(String hoscode,String sign){
+        //调用接口获取自己本地签名
+        String hospSign = hospitalSetService.getSignKey(hoscode);
+        //本地签名使用MD5加密
+        String hospSignMD5 = MD5.encrypt(hospSign);
+        //签名校验
+        if(!hospSignMD5.equals(sign)){
+            throw new YyghException(20001,"签名校验失败");
+        }
     }
 
 }
